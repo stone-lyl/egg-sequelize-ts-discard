de732eec18cccc60a5fc4bf326d9a71014a9b2b1
stone <stone-lyl@outlook.com>
2018-11-08 23:41:12 +0800

    fix: correct type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,26 @@
-import * as sequelize from "sequelize";
+import { Sequelize, ISequelizeValidationOnlyConfig } from 'sequelize-typescript';
+import { SequelizeConfig } from "sequelize-typescript/lib/types/SequelizeConfig";
 
 declare module 'egg' {
+    /**
+     * @see https://github.com/whxaxes/egg-ts-helper/issues/8
+     */
+    interface IModel extends Sequelize { }
 
-  // extend app
-  interface Application {
-    Sequelize: sequelize.SequelizeStatic;
-    model: sequelize.Sequelize;
-  }
+    // extend app
+    interface Application {
+        Sequelize: Sequelize;
+        model: IModel;
+    }
 
-  // extend context
-  interface Context {
-    model: sequelize.Sequelize;
-  }
+    // extend context
+    interface Context {
+        model: IModel;
+    }
 
-  // extend your config
-  interface EggAppConfig {
-    sequelize: sequelize.Options;
-  }
+    // extend your config
+    interface EggAppConfig {
+        sequelize: SequelizeConfig | ISequelizeValidationOnlyConfig
+    }
 
 }

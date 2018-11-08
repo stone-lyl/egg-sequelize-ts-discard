12250ade32e13a5896832c14f5d7058ed3a4ce75
stone <stone-lyl@outlook.com>
2018-11-08 23:17:17 +0800

    feat: 魔改egg-sequelize的ts版本

--- config/config.default.js
+++ config/config.default.js
@@ -1,27 +1,25 @@
 'use strict';
 
 exports.sequelize = {
-  dialect: 'mysql',
-  database: '',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '',
+    dialect: 'mysql',
+    database: '',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    Sequelize: require('sequelize-typescript').Sequelize
 
-  // support customize your own Squelize
-  // Sequelize: require('sequelize'), // v5 or v3
-
-  // support multi datasources by config.sequelize.datasources
-  // datasources: [
-  //   {
-  //     delegate: 'model', // lood to `app[delegate]`
-  //     baseDir: 'model', // models in `app/${model}`
-  //     // other sequelize configurations
-  //   },
-  //   {
-  //     delegate: 'sequelize', // lood to `app[delegate]`
-  //     baseDir: 'sequelize', // models in `app/${model}`
-  //     // other sequelize configurations
-  //   },
-  // ],
+    // support multi datasources by config.sequelize.datasources
+    // datasources: [
+    //   {
+    //     delegate: 'model', // lood to `app[delegate]`
+    //     baseDir: 'model', // models in `app/${model}`
+    //     // other sequelize configurations
+    //   },
+    //   {
+    //     delegate: 'sequelize', // lood to `app[delegate]`
+    //     baseDir: 'sequelize', // models in `app/${model}`
+    //     // other sequelize configurations
+    //   },
+    // ],
 };
